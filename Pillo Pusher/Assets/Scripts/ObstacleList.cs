7b86cf43b649e8c49670669fe55b423bcbf20daa
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2015-10-26 09:25:26 +0100

    object spawn

--- Pillo Pusher/Assets/Scripts/ObstacleList.cs
+++ Pillo Pusher/Assets/Scripts/ObstacleList.cs
@@ -4,23 +4,39 @@ using System.Collections.Generic;
 
 public class ObstacleList : MonoBehaviour {
 
-	List<GameObject> obstacles = new List<GameObject>();
-
+	private List<GameObject> obstacles;
+	private List<Vector3> posObstacles;
+	
 	public GameObject obj1;
 	public GameObject obj2;
 	public GameObject obj3;
-
+	
+	private Vector3 pos1 = new Vector3(-100f,1.1f,3);
+	private Vector3 pos2 = new Vector3(-100f,1.1f,0);
+	private Vector3 pos3 = new Vector3(-100f,1.1f,-3);
+	
 	// Use this for initialization
-	public void Initialise  ()
+	public void Init()
 	{
 		obstacles = new List<GameObject> ();
 		obstacles.Add (obj1);
 		obstacles.Add (obj2);
 		obstacles.Add (obj3);
+		
+		posObstacles = new List<Vector3> ();
+		posObstacles.Add (pos1);
+		posObstacles.Add (pos2);
+		posObstacles.Add (pos3);
 	}
-
+	
 	public List<GameObject> returnObstacles()
 	{
 		return obstacles;
 	}
+	
+	public List<Vector3> returnPosObstacles()
+	{
+		return posObstacles;
+	}
+
 }
--- Pillo Pusher/Assets/Scripts/ObstacleSpawn.cs
+++ Pillo Pusher/Assets/Scripts/ObstacleSpawn.cs
@@ -3,18 +3,35 @@ using System.Collections;
 
 public class ObstacleSpawn : MonoBehaviour {
 
+	public ObstacleList obl;
+	public bool ritme;
+	public float time = 1f;
+	//public Vector3 velocity = new Vector3(5,0,0);
+	
 	// Use this for initialization
 	void Start () {
-
+		obl.Init();
+		ritme = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-	public void placeObstacles(GameObject parentground)
-	{
-
+		time -= Time.deltaTime; 
+		
+		if(time <= 0f){
+			ritme = true;
+		}
+		
+		if(ritme){
+			int typeObstacle = 0;
+			typeObstacle = Random.Range(0, obl.returnObstacles().Count);
+			GameObject obstacle = Instantiate(obl.returnObstacles()[typeObstacle], obl.returnPosObstacles()[typeObstacle], Quaternion.identity) as GameObject;
+			//obstacle.transform.position += velocity * Time.deltaTime;
+			obstacle.AddComponent<Rigidbody>();
+			obstacle.GetComponent<Rigidbody>().velocity = new Vector3(25,0,0);
+			obstacle.GetComponent<Rigidbody>().useGravity = false;
+			time = 1f;
+			ritme = false;
+		}
 	}
 }
