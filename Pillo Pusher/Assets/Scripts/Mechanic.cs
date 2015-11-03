2543bf926aa12e9478efe5928a9cabc3693c4be4
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2015-11-03 14:26:21 +0100

    Speaker Feedback

--- Pillo Pusher/Assets/Scripts/Mechanic.cs
+++ Pillo Pusher/Assets/Scripts/Mechanic.cs
@@ -7,17 +7,26 @@ public class Mechanic : MonoBehaviour {
 	// Use this for initialization
 	private int state;
 	private Vector3 nextpos;
+
 	public MoveCharacter charactermover;
+	public GameOverPanel gameoverpanel;
+	//public SpeakerFeedback speakerFeedback;
+
+	public Animator speaker1;
+	public Animator speaker2;
+
 	private bool p1pressing;
 	private bool p2pressing;
 	private bool p1pressed;
 	private bool p2pressed;
 	private bool hasjumped;
+
 	public GameObject pillo1feedback;
 	public GameObject pillo2feedback;
+
 	float pct1;
 	float pct2;
-	public GameOverPanel gameoverpanel;
+	
 	void Start () 
 	{
 		p1pressing = false;
@@ -28,6 +37,11 @@ public class Mechanic : MonoBehaviour {
 		state = 1;
 		nextpos = new Vector3 (40, 0, 0);
 
+		//speakerFeedback.SpeakerLeftIdle();
+		//speakerFeedback.SpeakerRightIdle();
+
+		speaker1.SetInteger("SwitchState", 0);
+		speaker2.SetInteger("SwitchState", 0);
 	}
 	
 	// Update is called once per frame
@@ -48,6 +62,8 @@ public class Mechanic : MonoBehaviour {
 			p1pressing = true;
 			p1pressed = false;
 			pillo1feedback.GetComponent<Renderer>().material.color = Color.green;
+			//speakerFeedback.SpeakerLeftShake();
+			speaker1.SetInteger("SwitchState", 1);
 		}
 		else
 			if((Input.GetKeyUp("a")||pct1 <= 0.2) && p1pressing)
@@ -56,6 +72,8 @@ public class Mechanic : MonoBehaviour {
 			hasjumped = false;
 			p1pressing = false;
 			pillo1feedback.GetComponent<Renderer>().material.color = Color.red;
+			//speakerFeedback.SpeakerLeftIdle();
+			speaker1.SetInteger("SwitchState", 0);
 		}
 
 		if(Input.GetKeyDown("d") || pct2 >= 0.5)
@@ -63,6 +81,8 @@ public class Mechanic : MonoBehaviour {
 			p2pressing = true;
 			p2pressed = false;
 			pillo2feedback.GetComponent<Renderer>().material.color = Color.green;
+			//speakerFeedback.SpeakerRightShake();
+			speaker2.SetInteger("SwitchState", 1);
 		}
 		else
 		if((Input.GetKeyUp("d") || pct2 <= 0.2) && p2pressing)
@@ -71,6 +91,8 @@ public class Mechanic : MonoBehaviour {
 			hasjumped = false;
 			p2pressing = false;
 			pillo2feedback.GetComponent<Renderer>().material.color = Color.red;
+			//speakerFeedback.SpeakerRightIdle();
+			speaker2.SetInteger("SwitchState", 0);
 		}
 
 		if(p1pressing && p2pressing && !charactermover.isJumping())
