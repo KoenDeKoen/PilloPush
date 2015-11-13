bb02e4be4142daa89018c359693a7cb974630369
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2015-11-13 14:41:36 +0100

    FixedIT

--- Pillo Pusher/Assets/Scripts/CollisionPlayer.cs
+++ Pillo Pusher/Assets/Scripts/CollisionPlayer.cs
@@ -2,76 +2,96 @@
 using System.Collections;
 
 public class CollisionPlayer : MonoBehaviour {
-
 	public GameOverPanel gameoverpanel;
-
-    private int lives;
+	private int lives;
 	public float realTime;
-    private Animator charanimator;
-    private Mechanic mc;
 
+	private Animator charanimator;
+	private Mechanic mc;
+	private GrannyMechanic gmc;
+	private ModeSelect ms;
+	private Lives lifescript;
+			
 	public static bool tutorial = false;
 	public static bool activeSlow;
 	public bool slowdown;
-
-    void Start()
-    {
-        mc = FindObjectOfType<Mechanic>();
-        charanimator = FindObjectOfType<CollisionPlayer>().gameObject.GetComponentInChildren<Animator>();
-        lives = 3;
+		
+	void Start()		
+	{
+		lifescript = gameObject.AddComponent<Lives>();
+		//gameoverpanel.Init();
+		ms = gameObject.AddComponent<ModeSelect>();
+		
+		if (ms.getMode() == 1)	
+		{
+			mc = FindObjectOfType<Mechanic>();
+		}
+		
+		if (ms.getMode() == 2)
+		{
+			gmc = FindObjectOfType<GrannyMechanic>();
+		}
+		
+		charanimator = FindObjectOfType<CollisionPlayer>().gameObject.GetComponentInChildren<Animator>();
+		lives = 3;
 		realTime = 0f;
 		slowdown = false;
 		activeSlow = false;
-    }
+	}
 
 	void Update()
 	{
 		if(slowdown)
 		{
-			realTime += Time.deltaTime;
-			
-			if(Time.timeScale >= 1.0f)
-			{
-				Time.timeScale = 0.5f;
-			}
-			
-			if(realTime >= 6.0f)
-			{
-				Time.timeScale = 1.0f;
-				realTime = 0f;
-				activeSlow = false;
-				slowdown = false;
-			}
+			SlowMotion();
 		}
 	}
 
-	void OnCollisionEnter(Collision col)
+	void OnCollisionEnter(Collision col)		
 	{
-		if(col.gameObject.tag == "Car")
-		{
+		if(col.gameObject.tag == "Car")	
+		{	
 			LosLife();
-        }
+		}
 
 		if(col.gameObject.tag == "Bar")
 		{
 			LosLife();
-        }
+		}
 
 		if(col.gameObject.tag == "Light")
 		{
 			LosLife();
-        }
+		}
 
 		if(col.gameObject.tag == "Bal")
 		{
 			LosLife();
-        }
+		}
 
 		if(col.gameObject.tag == "slow")
 		{
-			activeSlow = true;
 			slowdown = true;
+			activeSlow = true;
 			Destroy(col.gameObject);
+		}
+	}
+
+	public void SlowMotion()	
+	{
+		realTime += Time.deltaTime;
+
+		if(Time.timeScale >= 1.0f)
+		{
+			Time.timeScale = 0.5f;
+		}
+
+		if(realTime >= 6.0f)
+		{
+			Time.timeScale = 1.0f;
+			realTime = 0f;
+			slowdown = false;
+			activeSlow = false;
 		}
 	}
 
@@ -80,22 +100,56 @@ public class CollisionPlayer : MonoBehaviour {
 		if(!tutorial)
 		{
 			lives--;
-			if (lives <= 0)
+			if (lives <= 0)	
 			{
-            	charanimator.SetInteger("State", 6);
-            	mc.animstate = "Gameover1";
+				if (ms.getMode() == 1)
+				{
+					mc.animstate = "Gameover1";
+				}
+				
+				if (ms.getMode() == 2)
+				{
+					gmc.animstate = "Gameover1";
+				}
+				
+				charanimator.SetInteger("State", 6);
 				gameoverpanel.displayScore();
 			}
-        	else
-       		{
-            charanimator.SetInteger("State", 5);
-           	mc.animstate = "Hit1";
-        	}
+			else
+			{
+				if (ms.getMode() == 1)
+				{
+					mc.animstate = "Hit1";
+				}
+				
+				if (ms.getMode() == 2)
+				{
+					gmc.animstate = "Hit1";
+				}
+				
+				charanimator.SetInteger("State", 5);
+			}
+			lifescript.setLives(lives);
 		}
+		
 		if(tutorial)
 		{
+			if (ms.getMode() == 1)
+			{
+				mc.animstate = "Hit1";
+			}
+			
+			if (ms.getMode() == 2)
+			{
+				gmc.animstate = "Hit1";
+			}
+			
 			charanimator.SetInteger("State", 5);
-			mc.animstate = "Hit1";
+			//mc.animstate = "Hit1";
 		}
 	}
 }
+
+
+
+
