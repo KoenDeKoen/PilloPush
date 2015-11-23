e0a84fb37878c2d4f9b300068948cf27f5145c93
KoenDeKoen <koen.brouwers@live.nl>
2015-11-23 14:20:00 +0100

    WellThen

--- Pillo Pusher/Assets/Scripts/PilloIntro.cs
+++ Pillo Pusher/Assets/Scripts/PilloIntro.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Pillo;
 
 public class PilloIntro : MonoBehaviour {
 
@@ -8,15 +9,25 @@ public class PilloIntro : MonoBehaviour {
     public Texture pillo1pressed;
     public Texture pillo2notpressed;
     public Texture pillo2pressed;
+    public GameObject pillo1image;
+    public GameObject pillo2image;
+    public GameObject timertext;
     
 	void Start ()
     {
-	
-	}
+        ConfigureSensorRange(0x50, 0x6f);
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+	    
 	}
+
+    public static void ConfigureSensorRange(int min, int max)
+    {
+        PilloSender.SensorMin = min;
+        PilloSender.SensorMax = max;
+    }
 }
