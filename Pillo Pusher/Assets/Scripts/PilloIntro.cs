79b14e4a952589881191fc3d95b99f5b9956226f
KoenDeKoen <koen.brouwers@live.nl>
2015-11-23 13:55:31 +0100

    StartOnPilloIntro

---
+++ Pillo Pusher/Assets/Scripts/PilloIntro.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PilloIntro : MonoBehaviour {
+
+    // Use this for initialization
+    public Texture pillo1notpressed;
+    public Texture pillo1pressed;
+    public Texture pillo2notpressed;
+    public Texture pillo2pressed;
+    
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
