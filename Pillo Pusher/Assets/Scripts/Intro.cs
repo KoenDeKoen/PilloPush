33ff2f8456e50becee8896537d1f43697d1351b9
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2015-11-27 13:52:06 +0100

    IntroSecondShit

--- Pillo Pusher/Assets/Scripts/Intro.cs
+++ Pillo Pusher/Assets/Scripts/Intro.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Intro : MonoBehaviour {
-	
+
+	public Settings settings;
+
 	void Start ()
 	{
-
+		settings.setAudioState(true);
 	}
 	
 	// Update is called once per frame
